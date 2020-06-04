9482960371acd710b568b3832f1be5f17293b8ab
Emma NWAMAIFE <emadimabua@gmail.com>
2020-06-04 13:12:10 +0100

    refactor: modify seeders to facilitate testing

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,7 +13,13 @@ class UsersTableSeeder extends Seeder
     {
         $users = App\User::pluck('id');
         if ($users->count() < 15) {
-            factory(App\User::class, (15 - $users->count()))->create();
+            factory(App\User::class, (15 - $users->count()))->create()->each(function ($user) {
+                if ($user->is_patient) {
+                    $user->patient()->save(factory(App\Patient::class)->make());
+                } else if ($user->is_specialist) {
+                    $user->specialist()->save(factory(App\Specialist::class)->make());
+                }
+            });
         }
     }
 }
