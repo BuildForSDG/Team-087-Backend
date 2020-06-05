e5b44c1673710495df9ec34ce6684ae655c20e70
Emma NWAMAIFE <emadimabua@gmail.com>
2020-06-05 16:33:46 +0100

    feat: add results-filter to returned data
    - added implementation for data filtering
    - applied pagination to returned data
    
    Closes #37

--- src/Http/Controllers/UserController.php
+++ src/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use Illuminate\Http\Request;
 
 /**
  * User Controller
@@ -11,7 +12,7 @@ use App\User;
  */
 class UserController extends Controller
 {
-    public function fetch()
+    public function fetch(Request $request)
     {
         if (!auth()->user()->is_admin) {
             return response()->json([
@@ -21,7 +22,27 @@ class UserController extends Controller
         }
 
         try {
-            return response()->json(['status' => true, 'data' => User::all()]);
+            $filters = [];
+            if ($request->has('active')) {
+                $filters['is_active'] = boolval($request->query('active', 0));
+            }
+
+            if ($request->has('patient')) {
+                $filters['is_patient'] = boolval($request->query('patient', 0));
+            }
+
+            if ($request->has('specialist')) {
+                $filters['is_specialist'] = boolval($request->query('specialist', 0));
+            }
+
+            if ($request->has('admin')) {
+                $filters['is_admin'] = boolval($request->query('admin', 0));
+            }
+
+            $perPage = $request->query('chunk', 10); //chunk-size of fetched-data
+            return response()->json([
+                'status' => true, 'data' => User::with(['patient', 'specialist'])->where($filters)->paginate($perPage)
+            ]);
         } catch (\Exception $e) {
             return response()->json([
                 'status' => false, 'message' => 'User(s) could not be fetched', 'errors' => ['error' => $e->getMessage()]
