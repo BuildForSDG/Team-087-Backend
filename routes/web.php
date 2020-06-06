579ee07d7dc2a819eb2052e5d77c4d5b54c722f6
Emma NWAMAIFE <emadimabua@gmail.com>
2020-06-06 15:10:50 +0100

    chore: minor bug-fix for fetch-reviews feature
    - fixes cause of failed travis-CI test on fetch-review feature
    - fixes shadow-route issues
    - renamed controller-method for consistency

--- routes/web.php
+++ routes/web.php
@@ -25,17 +25,20 @@ $router->group(['prefix' => 'api/v1'], function () use ($router) {
     });
 
     $router->group(['prefix' => 'users', 'middleware' => 'auth'], function () use ($router) {
+        $router->get('appointments', ['uses' => 'AppointmentController@fetch', 'as' => 'appointments.fetch']);
+
         $router->group(['prefix' => '{id}'], function () use ($router) {
-            $router->post('reviews', ['uses' => 'ReviewController@add', 'as' => 'review.add']);
-            $router->put('reviews/{reviewId}', ['uses' => 'ReviewController@edit', 'as' => 'review.edit']);
-            $router->get('reviews', ['uses' => 'ReviewController@view', 'as' => 'review.view']);
+            $router->post('reviews', ['uses' => 'ReviewController@add', 'as' => 'reviews.add']);
+            $router->put('reviews/{reviewId}', ['uses' => 'ReviewController@edit', 'as' => 'reviews.edit']);
+            $router->get('reviews', ['uses' => 'ReviewController@fetch', 'as' => 'reviews.fetch']);
 
             $router->post('appointments', ['uses' => 'AppointmentController@book', 'as' => 'appointments.book']);
             $router->get('appointments', ['uses' => 'AppointmentController@fetch', 'as' => 'appointments.fetch']);
+
+            $router->get('', ['uses' => 'UserController@view', 'as' => 'users.view']);
         });
 
         $router->get('', ['uses' => 'UserController@fetch', 'as' => 'users.fetch']);
-        $router->get('appointments', ['uses' => 'AppointmentController@fetch', 'as' => 'appointments.fetch']);
     });
 });
 
