aaad5c9580e721128b40423bdcb8be17cfacb2b9
Emma NWAMAIFE <emadimabua@gmail.com>
2020-06-11 02:07:05 +0100

    chore: fix for heroku-deployment issues
    - implements a configuration file for deployment to heroku

---
+++ config/database.php
@@ -0,0 +1,38 @@
+<?php
+
+$url = parse_url(env('DATABASE_URL'));
+
+return [
+    'default' => env('DB_CONNECTION', 'mysql'),
+
+    'connections' => [
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => $url['host'],
+            'port' => $url['port'],
+            'database' => ltrim($url['path'], '/'),
+            'username' => $url['user'],
+            'password' => $url['pass'],
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => env('DB_CHARSET', 'utf8'),
+            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'prefix' => env('DB_PREFIX', ''),
+            'strict' => env('DB_STRICT_MODE', true),
+            'engine' => env('DB_ENGINE', null),
+            'timezone' => env('DB_TIMEZONE', '+00:00'),
+        ],
+
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'host' => $url['host'],
+            'port' => $url['port'],
+            'database' => ltrim($url['path'], '/'),
+            'username' => $url['user'],
+            'password' => $url['pass'],
+            'charset' => env('DB_CHARSET', 'utf8'),
+            'prefix' => env('DB_PREFIX', ''),
+            'schema' => env('DB_SCHEMA', 'public'),
+            'sslmode' => env('DB_SSL_MODE', 'prefer'),
+        ],
+    ]
+];
