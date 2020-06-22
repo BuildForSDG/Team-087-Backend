a85de2589b48b689031f082167ef19450af4c642
Emma NWAMAIFE <emadimabua@gmail.com>
2020-06-23 00:50:54 +0100

    chore: add generic handler for validation errors

--- src/Exceptions/Handler.php
+++ src/Exceptions/Handler.php
@@ -49,6 +49,13 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if ($exception instanceof ValidationException) {
+            return response()->json([
+                'status' => false, 'message' => "Validation failed ({$exception->getMessage()})",
+                'errors' => $exception->errors(),
+            ], $exception->status);
+        }
+
         return parent::render($request, $exception);
     }
 }
